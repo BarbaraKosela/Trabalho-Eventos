cf1bbc95d2f9db34b8c5ef793b4e37038e13dd15
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-06-01 14:46:35 -0300

    EstatisticaConvidaospronto

--- TrabalhoEventos/TrabalhoEventos/EstatisticaConvidados.Designer.cs
+++ TrabalhoEventos/TrabalhoEventos/EstatisticaConvidados.Designer.cs
@@ -35,6 +35,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,34 +55,37 @@
             // 
             this.button2.BackColor = System.Drawing.Color.DarkRed;
             this.button2.Font = new System.Drawing.Font("Square721 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(77, 148);
+            this.button2.Location = new System.Drawing.Point(77, 135);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(169, 49);
             this.button2.TabIndex = 1;
             this.button2.Text = "TOTAL DE CADASTRO EQUIPAMENTO";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.DarkRed;
             this.button3.Font = new System.Drawing.Font("Square721 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(77, 224);
+            this.button3.Location = new System.Drawing.Point(77, 203);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(169, 49);
             this.button3.TabIndex = 2;
             this.button3.Text = "TOTAL DE CADASTRO ATENDIMENTO JOGADOR";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.DarkRed;
             this.button4.Font = new System.Drawing.Font("Square721 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(77, 302);
+            this.button4.Location = new System.Drawing.Point(77, 270);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(169, 49);
             this.button4.TabIndex = 3;
             this.button4.Text = "TOTAL DE CADASTRO EQUIPE";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // pictureBox1
             // 
@@ -97,18 +101,32 @@
             // 
             this.button5.BackColor = System.Drawing.Color.DarkRed;
             this.button5.Font = new System.Drawing.Font("Square721 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(77, 391);
+            this.button5.Location = new System.Drawing.Point(77, 341);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(169, 49);
             this.button5.TabIndex = 5;
             this.button5.Text = "TOTAL DE CADASTROS";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DarkRed;
+            this.button6.Font = new System.Drawing.Font("Square721 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(77, 447);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(169, 49);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "VOLTAR A PAGINA ANTERIOR";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // EstatisticaConvidados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(960, 548);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button4);
@@ -131,5 +149,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
